a33543bdf7ae437b54a24c8dfdd93c11b81d8b9f
Naheed <naheedcse@gmail.com>
2017-05-24 12:07:12 +0800

    change

--- src/Hypergraphtestmain.java
+++ src/Hypergraphtestmain.java
@@ -5,7 +5,7 @@ public class Hypergraphtestmain {
     public static void main(String[] args){
         String pathtowrite = "/Users/naheed/IdeaProjects/TD1/RandomSimpComplex/output";
 //        String pathtowrite = "C:\\RandomSimpComplex\\randsimpcomp\\RandomSimpComplex\\output";
-        int N = 5;
+        int N = 20;
 //        RandomHypergraph rsc = new RandomHypergraph(N,(float)0.5);
 //        rsc.generate();
 //        rsc.Write(pathtowrite);
@@ -31,37 +31,31 @@ public class Hypergraphtestmain {
         /**
          * k-subset test , Varying probability analytics
          */
-//        SpernerFamilyAnalytics sfa_varyp = new SpernerFamilyAnalytics(N);
-//        sfa_varyp.setSimplextype("hg");
-//        sfa_varyp.runforp(100,new double[]{0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1});
-////        sfa_varyp.runforp(100,new double[]{1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0});
-//        sfa_varyp.WritevarypSpernSize(pathtowrite);
+        SpernerFamilyAnalytics sfa_varyp = new SpernerFamilyAnalytics(N);
+        sfa_varyp.setSimplextype("hg");
+        sfa_varyp.runforp(100,new double[]{0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1});
+//        sfa_varyp.runforp(100,new double[]{1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0});
+        sfa_varyp.WritevarypSpernSize(pathtowrite);
 
         /**
          * Scalability test for baseline hypergraph and Z-algorithm
          */
-        String[] type = new String[]{"baseh","hg"};
-        for(String typ:type) {
-            int[] vertices_array = new int[]{5, 10, 15, 20, 25};
-            Scalability sb = new Scalability(vertices_array, new float[]{0.1f});
-            sb.setSimplextype(typ);
-            sb.runforNrunforp(100);
-            sb.WriteforNforp(pathtowrite);
-        }
-
-        for(String typ:type) {
-            int[] vertices_array = new int[]{20};
-            Scalability sb = new Scalability(vertices_array, new float[]{0,0.1f,0.2f,0.3f,0.4f,0.5f,0.6f,0.7f,0.8f,0.9f,1});
-            sb.setSimplextype(typ);
-            sb.runforNrunforp(100);
-            sb.WriteforNforp(pathtowrite);
-        }
-
-        // Baseline hg
+//        String[] type = new String[]{"baseh","hg"};
+//        for(String typ:type) {
+//            int[] vertices_array = new int[]{5, 10, 15, 20, 25};
+//            Scalability sb = new Scalability(vertices_array, new float[]{0.1f});
+//            sb.setSimplextype(typ);
+//            sb.runforNrunforp(100);
+//            sb.WriteforNforp(pathtowrite);
+//        }
 //
-//        Scalability sb = new Scalability(vertices_array,0.01,10);
-//        sb.setSimplextype("basehg");
-//        sb.runforN();
-//        sb.WriteRuntimes(pathtowrite);
+//        for(String typ:type) {
+//            int[] vertices_array = new int[]{10};
+//            Scalability sb = new Scalability(vertices_array, new float[]{0,0.1f,0.2f,0.3f,0.4f,0.5f,0.6f,0.7f,0.8f,0.9f,1});
+//            sb.setSimplextype(typ);
+//            sb.runforNrunforp(100);
+//            sb.WriteforNforp(pathtowrite);
+//        }
+
     }
 }
