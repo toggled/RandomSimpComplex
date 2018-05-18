f31b7da99dd68b574d24a0777e7528a0178479a4
Naheed <naheedcse@gmail.com>
2018-05-18 19:15:30 +0800

    String representation of a simplicial complex changed. MD5 representation added.

--- src/SimplicialComplex.java
+++ src/SimplicialComplex.java
@@ -5,6 +5,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import org.joda.time.DateTime;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by naheed on 5/17/17.
@@ -36,11 +38,18 @@ public abstract class SimplicialComplex extends HyperGraph implements WriteHandl
     }
 
     public String toString(){
-        System.out.println("The simplicial complex: ");
-        String s = "";
-        for(BitSet b:simplices)
-            s+=(bitsettoString(b)+"\n");
-        return s;
+//        System.out.println("The simplicial complex: ");
+//        String s = "";
+//        for(BitSet b:simplices)
+//            s+=(bitsettoString(b)+"\n");
+//        return s;
+
+        if (stringrep == null) {
+            stringrep = "";
+            for (BitSet b : simplices)
+                stringrep += (bitsettoString(b) + "/");
+        }
+        return stringrep.substring(0,stringrep.length()-1);
     }
 
     public String numOfkSimplicesAsString(){
@@ -66,7 +75,6 @@ public abstract class SimplicialComplex extends HyperGraph implements WriteHandl
     public void Write(String path){
         String toappend = "("+Integer.toString(this.numberOfVertices)+","+Integer.toString(this.simplices.size())+")"+new DateTime( GregorianCalendar.getInstance().getTime() ).toString("yyyy-MM-dd HH-mm-ss");
 
-
         try {
             Files.write(Paths.get(path+"/"+toappend+".txt"),this.toString().getBytes());
         } catch (IOException e) {
@@ -83,4 +91,27 @@ public abstract class SimplicialComplex extends HyperGraph implements WriteHandl
         }
     }
     abstract void generate();
+    void clear(){
+        simplices.clear();
+        numOfkSimplices = null;
+    }
+
+    protected String getMD5repr(){
+        MessageDigest messageDigest;
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-1");
+            messageDigest.update(this.toString().getBytes());
+            byte[] messageDigestMD5 = messageDigest.digest();
+
+            for (byte bytes : messageDigestMD5) {
+                stringBuffer.append(String.format("%02x", bytes & 0xff));
+            }
+
+        }catch (NoSuchAlgorithmException exception) {
+            exception.printStackTrace();
+
+        }
+        return stringBuffer.toString();
+    }
 }
